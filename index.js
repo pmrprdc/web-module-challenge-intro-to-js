2b5abf195a480770c07b4464358c4a6b2e1d542e
Pamir Perdeci <pamirperdeci@gmail.com>
2022-08-08 15:34:19 -0400

    solved dog years & passing 3 tests now

--- index.js
+++ index.js
@@ -77,11 +77,10 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(dogAge){
+  return dogAge*7;
+
 }
-
-
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
